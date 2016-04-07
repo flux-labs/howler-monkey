8700f2a664199987b455b8b183f8412f88dad6c5
felix <felix@flux.io>
2016-04-07 16:14:25 -0700

    Parsing real mesh

--- backend/output-rays/main.cpp
+++ backend/output-rays/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <cstdio>
-#include <cstring>
+#include <string>
 #include "../../dist/include/jsoncpp/json.h"
 
 using namespace std;
@@ -8,6 +7,7 @@ using namespace std;
 int main(int argc, char* argv[])
 {
   //parsing mesh
+  //Mesh has to have no whitespace and escaped quotation marks
   string mesh = argv[1];
 
   Json::Value root;
@@ -22,15 +22,28 @@ int main(int argc, char* argv[])
   }
 
   const Json::Value faces = root["faces"];
+  const Json::Value vertices = root["vertices"];
 
   for(unsigned int i=0; i < faces.size(); i++) {
       Json::Value face = faces[i];
-      cout << "Face " << i << " in array: "
+      cout << "Face " << i << " in mesh: "
       << face[0].asString()
       << ", "
       << face[1].asString()
       << ", "
       << face[2].asString()
+      << endl;
+  }
+
+
+  for(unsigned int i=0; i < vertices.size(); i++) {
+      Json::Value vertex = vertices[i];
+      cout << "Vertex " << i << " in mesh: "
+      << vertex[0].asString()
+      << ", "
+      << vertex[1].asString()
+      << ", "
+      << vertex[2].asString()
       << endl;
   }
 

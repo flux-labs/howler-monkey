6c6e9d3f9913a98cd002de7e779a860e3acd381b
felix <felix@flux.io>
2016-04-08 10:26:02 -0700

    Fixed impulse

--- backend/impulse_response.cpp
+++ backend/impulse_response.cpp
@@ -10,7 +10,7 @@
 #define SPEED_OF_SOUND 340.29
 
 const int SECONDS_OF_IR = 6;
-const size_t SIZE = 16777216;
+const size_t SIZE = 262144;
 const int MAX = 1020;
 const int HALF_MAX = MAX / 2;
 
@@ -85,6 +85,7 @@ void create_impulse(std::vector<interaction> interactions, std::string fileName)
 
     auto fft = Aquila::FftFactory::getFft(SIZE);
     Aquila::SpectrumType impulseSpectrum = fft->fft(impulse);
+
 
     for (std::vector<interaction>::const_iterator i = interactions.begin(); i != interactions.end(); ++i) {
         apply_reverb(res, impulseSpectrum, i, fft);

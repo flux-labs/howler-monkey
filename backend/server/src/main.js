e88445018f5b91e8ba61e3488b2040723c3e455e
DnMllr <me@dnmllr.io>
2016-04-07 22:48:47 -0700

    made everything simpler

--- backend/server/src/main.js
+++ backend/server/src/main.js
@@ -1,68 +1,60 @@
-import Cycle from '@cycle/core';
-import {makeDOMDriver, hJSX} from '@cycle/dom';
 
 
-const inputIds = ['#source', '#listener'];
-const location = '-location input.';
-const dimensions = ['x', 'y', 'z'];
-const queries = inputIds.reduce((memo, input) => {
-  return memo.concat(dimensions.map(dim => input + location + dim))
-}, []);
+function main() {
+
+  var inputs = ['#source-location input.', '#listener-location input.'];
+  var dimensions = ['x', 'y', 'z']
+  var queries = inputs.reduce(function(acc, input) {
+    return acc.concat(dimensions.map(function(dim) {
+      return input + dim;
+    }));
+  }, []);
 
 
-function _isValidInputState(state) {
-  var keys = Object.keys(state);
-  return keys.length  === 6 && keys.reduce(function(acc, key) {
-    return acc && !isNaN(state[key].value);
-  }, true);
+  function _queryToKey(query) {
+    if (~query.indexOf('source')) {
+      return {
+        key: 'source',
+        dim: query[query.length - 1]
+      };
+    } else {
+      return {
+        key: 'listener',
+        dim: query[query.length - 1]
+      };
+    }
+  }
+
+  document.getElementById('obj-submit').addEventListener('click', function() {
+    var obj = document.getElementById('obj-entry').value;
+    if (!obj || ~obj.indexOf('enter a mesh here')) {
+      throw new Error('You must enter an obj');
+    }
+
+    var inputState = queries.reduce(function(acc, input) {
+      var el = document.querySelector(input);
+      var key = _queryToKey(input);
+      acc[key.key][key.dim] = Number(el.value ? el.value : 'asdf');
+      if (isNaN(acc[key.key][key.dim])) {
+        throw new Error(input + ' must have a numeric value');
+      }
+      return acc;
+    }, {
+      source: {},
+      listener: {}
+    });
+
+    var res = JSON.stringify({
+      obj: obj,
+      points: inputState
+    });
+
+    console.log(res);
+  });
 }
 
-
-function main(drivers) {
-
-  const inputs = queries.map(query => drivers.DOM.select(query).events('keyup')
-  .map(ev => {
-    return {
-      value: Number(ev.target.value),
-      selector: query
-    };
-  }));
-
-  const inputValues = Rx.Observable.merge(inputs).scan((acc, x) => {
-    acc[x.selector] = x.value;
-    return acc;
-  }, {});
-
-  const submitClicks = drivers.DOM.select('#obj-submit').events('click');
-
-
-  return {
-    DOM: submitClicks.map(_ => _isValidInputState(inputState))
-    .startWith(true)
-    .map(valid =>
-      <div id="input-status">
-        <textarea className="text-entry">
-          enter a mesh here
-        </textarea>
-        <div id="source-location">
-          <input className="x" type="text" placeholder="x"></input>
-          <input className="y" type="text" placeholder="y"></input>
-          <input className="z" type="text" placeholder="z"></input>
-        </div>
-        <div id="listener-location">
-          <input className="x" type="text" placeholder="x"></input>
-          <input className="y" type="text" placeholder="y"></input>
-          <input className="z" type="text" placeholder="z"></input>
-        </div>
-        <button id="obj-submit">send obj</button>
-      </div>
-    )
-  };
+if (document.body) {
+  main();
+} else {
+  document.addEventListener('DOMContentLoaded', main);
 }
-
-const drivers = {
-  DOM: makeDOMDriver('#app')
-}
-
-Cycle.run(main, drivers);
-

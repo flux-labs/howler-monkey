225ffec5c8bc0cdf11848da6d13371e556544ff1
felix <felix@flux.io>
2016-04-07 20:36:06 -0700

    Working reflections

--- backend/output-rays/main.cpp
+++ backend/output-rays/main.cpp
@@ -3,11 +3,26 @@
 #include "../../dist/include/jsoncpp/json.h"
 #include <ctime>
 #include <cstdlib>
+#include <math.h>
 
 //JUST FOR DEBUGGING TAKE ME OUT PLEASE
-#include <sstream>
+#include <stdio.h>
 
 using namespace std;
+
+void print_vector3 (float * vector, string name) {
+  cout << "testing " << name
+  <<endl;
+
+  for (int i = 0; i < 3; i++) {
+      printf("%lf\n", vector[i]);
+  }
+
+  printf("\n");
+}
+
+#define NUMBER_OF_RAYS 1
+#define MAXIMUM_REFLECTIONS 10
 
 #define crossProduct(a,b,c) \
   (a)[0] = (b)[1] * (c)[2] - (c)[1] * (b)[2]; \
@@ -23,6 +38,25 @@ using namespace std;
   (a)[0] = (b)[0] - (c)[0]; \
   (a)[1] = (b)[1] - (c)[1]; \
   (a)[2] = (b)[2] - (c)[2];
+
+void reflect_ray(float *direction, float *normal, float *new_direction) {
+  float direction_dot_normal = innerProduct(direction, normal);
+
+  new_direction[0] = 2 * (direction_dot_normal * normal[0]) - 1;
+  new_direction[1] = 2 * (direction_dot_normal * normal[1]) - 1;
+  new_direction[2] = 2 * (direction_dot_normal * normal[2]) - 1;
+}
+
+float vector_length(float *vector) {
+  return sqrt(vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2]);
+}
+
+void normalize_vector(float *vector) {
+  float length = vector_length(vector);
+  vector[0] /= length;
+  vector[1] /= length;
+  vector[2] /= length;
+}
 
 int rayIntersectsTriangle(float *p, float *d, float *v0, float *v1, float *v2) {
   float e1[3],e2[3],h[3],s[3],q[3];
@@ -66,38 +100,95 @@ Json::Value parse_mesh(string mesh) {
       <<endl;
   }
 
-  const Json::Value faces = root["faces"];
-  const Json::Value vertices = root["vertices"];
-
-  for (unsigned int i=0; i < faces.size(); i++) {
-      Json::Value face = faces[i];
-      cout << "Face " << i << " in mesh: "
-      << face[0].asString()
-      << ", "
-      << face[1].asString()
-      << ", "
-      << face[2].asString()
-      << endl;
-  }
-
-  for (unsigned int i=0; i < vertices.size(); i++) {
-      Json::Value vertex = vertices[i];
-      cout << "Vertex " << i << " in mesh: "
-      << vertex[0].asString()
-      << ", "
-      << vertex[1].asString()
-      << ", "
-      << vertex[2].asString()
-      << endl;
-  }
-
   return root;
 }
 
-int main(int argc, char* argv[])
-{
-  string mesh = argv[1];
+void run_simulation(float *listener_position, string mesh) {
   Json::Value parsed_mesh = parse_mesh(mesh);
+  const Json::Value vertices = parsed_mesh["vertices"];
+  const Json::Value faces = parsed_mesh["faces"];
+
+  float position[3] = {
+    listener_position[0],
+    listener_position[1],
+    listener_position[2]
+  };
+
+  float direction[3] = {
+    0,
+    0,
+    0
+  };
+
+  for (int k = 0; k < NUMBER_OF_RAYS; k++) {
+    int bounces = 0;
+
+    direction[0] = (static_cast<float>(rand())/static_cast<float>(RAND_MAX)) * 2.0f - 1.0f;
+    direction[1] = (static_cast<float>(rand())/static_cast<float>(RAND_MAX)) * 2.0f - 1.0f;
+    direction[2] = (static_cast<float>(rand())/static_cast<float>(RAND_MAX)) * 2.0f - 1.0f;
+
+
+
+    while (bounces < MAXIMUM_REFLECTIONS) {
+
+      for (unsigned int i=0; i < faces.size(); i++) {
+        Json::Value face = faces[i];
+
+        float v0[3] = {
+          vertices[face[0].asUInt()][0].asFloat(),
+          vertices[face[0].asUInt()][1].asFloat(),
+          vertices[face[0].asUInt()][2].asFloat()
+        };
+
+        float v1[3] = {
+          vertices[face[1].asUInt()][0].asFloat(),
+          vertices[face[1].asUInt()][1].asFloat(),
+          vertices[face[1].asUInt()][2].asFloat()
+        };
+
+        float v2[3] = {
+          vertices[face[2].asUInt()][0].asFloat(),
+          vertices[face[2].asUInt()][1].asFloat(),
+          vertices[face[2].asUInt()][2].asFloat()
+        };
+
+        int intersects = rayIntersectsTriangle(position, direction, v0, v1, v2);
+
+        //reflect if intersection
+        if (intersects) {
+          cout << "Intersects with face " << i
+          << endl;
+
+          float new_direction[3] = {0, 0, 0};
+
+          //figuring out normal
+          //TODO: process the mesh and create a buffer of normals to reuse in ray intersects mesh function
+          float normal[3] = {0, 0, 0};
+          float e1[3], e2[3];
+          vector(e1, v1, v0);
+          vector(e2, v2, v0);
+          crossProduct(normal, e1, e2);
+          normalize_vector(normal);
+
+          reflect_ray(direction, normal, new_direction);
+
+          direction[0] = new_direction[0];
+          direction[1] = new_direction[1];
+          direction[2] = new_direction[2];
+
+          bounces++;
+        }
+      }
+
+    }
+
+
+  }
+
+}
+
+int main(int argc, char* argv[]) {
+  string mesh = argv[1];
 
   srand(time(0));
 
@@ -107,40 +198,7 @@ int main(int argc, char* argv[])
     0.0f
   };
 
-  float randomDirection[3] = {
-    static_cast<float>(rand())/static_cast<float>(RAND_MAX),
-    static_cast<float>(rand())/static_cast<float>(RAND_MAX),
-    static_cast<float>(rand())/static_cast<float>(RAND_MAX)
-  };
-
-  const Json::Value vertices = parsed_mesh["vertices"];
-  const Json::Value faces = parsed_mesh["faces"];
-  for (unsigned int i=0; i < faces.size(); i++) {
-    Json::Value face = faces[i];
-
-    float v0[3] = {
-      vertices[face[0].asUInt()][0].asFloat(),
-      vertices[face[0].asUInt()][1].asFloat(),
-      vertices[face[0].asUInt()][2].asFloat()
-    };
-
-    float v1[3] = {
-      vertices[face[1].asUInt()][0].asFloat(),
-      vertices[face[1].asUInt()][1].asFloat(),
-      vertices[face[1].asUInt()][2].asFloat()
-    };
-
-    float v2[3] = {
-      vertices[face[2].asUInt()][0].asFloat(),
-      vertices[face[2].asUInt()][1].asFloat(),
-      vertices[face[2].asUInt()][2].asFloat()
-    };
-
-    cout << "Intersects with face " << i
-    << ": "
-    << rayIntersectsTriangle(startingPosition, randomDirection, v0, v1, v2)
-    << endl;
-  }
+  run_simulation(startingPosition, mesh);
 
   return 0;
 }
